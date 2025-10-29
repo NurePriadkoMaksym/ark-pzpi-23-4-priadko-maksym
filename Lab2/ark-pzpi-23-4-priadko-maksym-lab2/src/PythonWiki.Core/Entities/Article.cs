0d4abefadaadcac813e9bb87a63f6c0f71d3ff78
Huddsto <maksym.priadko@nure.ua>
2025-10-29 14:26:30 +0200

    created backend base

---
+++ Lab2/ark-pzpi-23-4-priadko-maksym-lab2/src/PythonWiki.Core/Entities/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace PythonWiki.Core.Entities;
+
+public class Article
+{
+    public Guid Id { get; set; }
+    public string Slug { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string ContentMarkdown { get; set; } = null!;
+    public string? ContentHtml { get; set; }
+    public Guid AuthorId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Lab2/ark-pzpi-23-4-priadko-maksym-lab2/src/PythonWiki.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PythonWiki.Core.Entities;
+
+namespace PythonWiki.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Article> Articles => Set<Article>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        modelBuilder.Entity<Article>().HasIndex(a => a.Slug).IsUnique();
+    }
+}
